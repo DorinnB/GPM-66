66347d03cc1aa15b865c95fd9891caedb7cbc535
DorinnB <dorinn@netcourrier.com>
2017-05-03 14:39:01 +0200

    lab view add temperature

--- models/lab-model.php
+++ models/lab-model.php
@@ -9,22 +9,22 @@ class LabModel
     }
 
     public function getTest() {
-      $req='select info_jobs.customer, info_jobs.job, master_eprouvettes.prefixe, master_eprouvettes.nom_eprouvette, n_fichier, currentBlock, eprouvettes.Cycle_final, split, machine, poste, id_job, n_essai,
-        c_frequence, c_frequence_STL, c_cycle_STL, d_frequence, d_frequence_STL, Cycle_STL, runout
-        from enregistrementessais
-        left join eprouvettes on eprouvettes.id_eprouvette=enregistrementessais.id_eprouvette
-        left join master_eprouvettes on master_eprouvettes.id_master_eprouvette=eprouvettes.id_master_eprouvette
-        left join tbljobs on tbljobs.id_tbljob=eprouvettes.id_job
-        left join info_jobs on info_jobs.id_info_job=tbljobs.id_info_job
-        left join prestart on prestart.id_prestart=enregistrementessais.id_prestart
-        left join postes on postes.id_poste=prestart.id_poste
-        left join machines on machines.id_machine=postes.id_machine
+      $req='SELECT info_jobs.customer, info_jobs.job, master_eprouvettes.prefixe, master_eprouvettes.nom_eprouvette, n_fichier, currentBlock, eprouvettes.Cycle_final, split, machine, poste, id_job, n_essai,
+        c_frequence, c_frequence_STL, c_cycle_STL, d_frequence, d_frequence_STL, Cycle_STL, runout, c_temperature
+        FROM enregistrementessais
+        LEFT JOIN eprouvettes ON eprouvettes.id_eprouvette=enregistrementessais.id_eprouvette
+        LEFT JOIN master_eprouvettes ON master_eprouvettes.id_master_eprouvette=eprouvettes.id_master_eprouvette
+        LEFT JOIN tbljobs ON tbljobs.id_tbljob=eprouvettes.id_job
+        LEFT JOIN info_jobs ON info_jobs.id_info_job=tbljobs.id_info_job
+        LEFT JOIN prestart ON prestart.id_prestart=enregistrementessais.id_prestart
+        LEFT JOIN postes ON postes.id_poste=prestart.id_poste
+        LEFT JOIN machines ON machines.id_machine=postes.id_machine
 
-        where n_fichier in (SELECT max(n_fichier)
+        WHERE n_fichier in (SELECT max(n_fichier)
         FROM `enregistrementessais`
-        left join prestart on prestart.id_prestart=enregistrementessais.id_prestart
-        left join postes on postes.id_poste=prestart.id_poste
-        group by postes.id_machine)
+        LEFT JOIN prestart ON prestart.id_prestart=enregistrementessais.id_prestart
+        LEFT JOIN postes ON postes.id_poste=prestart.id_poste
+        GROUP BY postes.id_machine)
         order by machine';
         //echo $req;
         return $this->db->getAll($req);
@@ -35,17 +35,17 @@ class LabModel
         machine, t1.technicien as operateur, t2.technicien as controleur, currentBlock, tbljobs.id_tbljob
 
         FROM enregistrementessais
-        left join eprouvettes on eprouvettes.id_eprouvette=enregistrementessais.id_eprouvette
-        left join master_eprouvettes on master_eprouvettes.id_master_eprouvette=eprouvettes.id_master_eprouvette
-        left join tbljobs on tbljobs.id_tbljob=eprouvettes.id_job
-        left join info_jobs on info_jobs.id_info_job=tbljobs.id_info_job
-        left join prestart on prestart.id_prestart=enregistrementessais.id_prestart
-        left join postes on postes.id_poste=prestart.id_poste
-        left join machines on machines.id_machine=postes.id_machine
+        LEFT JOIN eprouvettes ON eprouvettes.id_eprouvette=enregistrementessais.id_eprouvette
+        LEFT JOIN master_eprouvettes ON master_eprouvettes.id_master_eprouvette=eprouvettes.id_master_eprouvette
+        LEFT JOIN tbljobs ON tbljobs.id_tbljob=eprouvettes.id_job
+        LEFT JOIN info_jobs ON info_jobs.id_info_job=tbljobs.id_info_job
+        LEFT JOIN prestart ON prestart.id_prestart=enregistrementessais.id_prestart
+        LEFT JOIN postes ON postes.id_poste=prestart.id_poste
+        LEFT JOIN machines ON machines.id_machine=postes.id_machine
         LEFT JOIN techniciens t1 ON t1.id_technicien=enregistrementessais.id_operateur
         LEFT JOIN techniciens t2 ON t2.id_technicien=enregistrementessais.id_controleur
 
-        where currentBlock="Check"
+        WHERE currentBlock="Check"
         order by machine';
         //echo $req;
         return $this->db->getAll($req);
--- views/lab-poste-large-view.php
+++ views/lab-poste-large-view.php
@@ -4,7 +4,7 @@
       <div class="col-md-12 <?= $poste[$n_poste]['currentBlock']  ?>" style="border:1px solid black; margin:5px 0px;">
         <b><?= $poste[$n_poste]['machine']  ?><br/></b>
         Job: <?= $poste[$n_poste]['customer'].' '.$poste[$n_poste]['job'].' '. $poste[$n_poste]['split']  ?><br/>
-        Files: <?= $poste[$n_poste]['n_fichier'].' Test: '.$poste[$n_poste]['n_essai']  ?><br/>
+        Files: <?= $poste[$n_poste]['n_fichier'].' Test: '.$poste[$n_poste]['n_essai'].' T°: '.(!empty($poste[$n_poste]['c_temperature'])?number_format($poste[$n_poste]['c_temperature'], 0,'.', ' '):'')  ?><br/>
         ID: <?= $poste[$n_poste]['prefixe'].' '.$poste[$n_poste]['nom_eprouvette']  ?><br/>
         <?= isset($poste[$n_poste]['Cycle_final'])?$poste[$n_poste]['Cycle_final'].'&nbsp;cycles&nbsp;('.$poste[$n_poste]['tempsRestant'].'&nbsp;h&nbsp;left)':''  ?><br/>
         <?= $poste[$n_poste]['currentBlock']  ?>
