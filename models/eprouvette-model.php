66be67b297858b0e9b8bb0151935c36b47a36708
DorinnB <dorinn@netcourrier.com>
2017-08-07 12:14:54 +0200

    retablissement calcul temps estimé sans tenir compte d_checked. A remettre plus tard

--- models/eprouvette-model.php
+++ models/eprouvette-model.php
@@ -618,6 +618,14 @@ class EprouvetteModel
         AND c_type_2_val = (SELECT c_type_2_val FROM eprouvettes WHERE id_eprouvette=' .$this->id.')
           AND cycle_final IS NOT NULL AND d_checked > 0
         GROUP by id_job, c_type_1_val, c_type_2_val';
+//d_checked>0 pour activer le calcul du temps uniquement si les données sont validé, essai terminé
+        $req='SELECT AVG(IF(cycle_estime IS NOT NULL, cycle_estime,cycle_final)) AS cycle_estime, c_type_1_val, c_type_2_val
+          FROM eprouvettes
+          WHERE id_job=(SELECT id_job FROM eprouvettes WHERE id_eprouvette=' .$this->id.')
+          AND c_type_1_val = (SELECT c_type_1_val FROM eprouvettes WHERE id_eprouvette=' .$this->id.')
+          AND c_type_2_val = (SELECT c_type_2_val FROM eprouvettes WHERE id_eprouvette=' .$this->id.')
+            AND cycle_final IS NOT NULL
+          GROUP by id_job, c_type_1_val, c_type_2_val';
 
         //echo $req.'<br/><br/>';
         return $this->db->getOne($req);
