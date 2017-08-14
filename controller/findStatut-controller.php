ea237cb72b84c9da1ee1e036ff6e8964aab69f23
DorinnB <dorinn@netcourrier.com>
2017-08-14 16:18:28 +0200

    findstatut upgrad

--- controller/findStatut-controller.php
+++ controller/findStatut-controller.php
@@ -26,16 +26,23 @@ if ($state['nbLocal'] > 0) {
 elseif ($state['nbSubC'] > 0) {
   $statut='awaiting SubC';
 }
-
-
 elseif ($state['checked']==1 AND $state['nbConsLeftAndInOut_A']>1 AND $state['nbTestLeft']>0) {
     $statut='ready to test';
+}
+elseif ($state['nbConsLeft']==0 AND $state['nbTestLeft']>0 AND $state['nbInOut_A']>0) {
+    $statut='inHouse but need condition';
 }
 /*
 if ($state['checked']==1 AND $state['nbConsLeft']>1) {
     $statut='ready to test SANS INOUT';
 }*/
 
+if ($state['nbConsLeftAux']>0 AND $state['nbTestLeft']>0 AND $state['nbInOut_A']>=$state['nbTestLeft']) {
+    $statut='Ready to test Aux';
+}
+elseif ($state['nbConsLeft']==0 AND $state['nbTestLeft']>0 AND $state['nbConsLeftAndInOut_A']>0) {
+    $statut='Testing on Hold Condition';
+}
 
 
 if ($state['running']>=1) {
@@ -49,10 +56,8 @@ if ($state['running']>=1) {
     $statut='running';
   }
 }
-elseif ($state['nbConsLeft']==0 AND $state['nbTestLeft']>0) {
-    $statut='Testing on Hold Condition';
 
-}
+
 
 
 if ($state['nbUnDChecked']==0 AND $state['nbTestLeft']==0) {
