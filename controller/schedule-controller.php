71c134bd75362570ec83b7cad0e3f0375e0585bb
DorinnB <dorinn@netcourrier.com>
2017-10-24 14:04:09 +0200

    .

--- controller/schedule-controller.php
+++ controller/schedule-controller.php
@@ -17,20 +17,46 @@ $groupes=$oWorkflow->getAllGroupes();
 //$eprouvettes=$oWorkflow->getAllGroupes();
 
 
+//on recupere un string des id_tbljobs dans l'ordre des groupes et on en fait un array
+foreach ($groupes as $key=>$val)  {
+  $groupes[$key]['split_explode']=explode(",", $val['ordre']);
+  foreach ($groupes[$key]['split_explode'] as $k => $v) {
+    $groupes[$key]['split'][$v]=1;
+  }
+}
 
-  foreach ($groupes as $key=>$val)  {
-    $groupes[$key]['split_explode']=explode(",", $val['ordre']);
-    foreach ($groupes[$key]['split_explode'] as $k => $v) {
-      $groupes[$key]['split'][$v]=1;
+//on crée un array split2 qui a pour clé son id_tbljob
+foreach ($splits as $key=>$val)  {
+  $split2[$splits[$key]['id_tbljob']]=$splits[$key];
+}
+
+//pour chaque groupe, on regarde la date par rapport a la date précédente
+foreach ($groupes as $key=>$val)  {
+  foreach ($groupes[$key]['split_explode'] as $k => $v) {
+    if ($k>0) { //uniquement pour le 2eme de la liste, on compare avec n-1
+      if ($split2[$groupes[$key]['split_explode'][$k]]['DyT_expected']<$split2[$groupes[$key]['split_explode'][$k-1]]['DyT_expected']) {
+        $split2[$groupes[$key]['split_explode'][$k]]['erreur_DyT_expected'] = '1';
+      }
+      if ($split2[$groupes[$key]['split_explode'][$k]]['DyT_SubC']<$split2[$groupes[$key]['split_explode'][$k-1]]['DyT_SubC'] OR $split2[$groupes[$key]['split_explode'][$k]]['DyT_SubC']<$split2[$groupes[$key]['split_explode'][$k-1]]['DyT_Cust']) {
+        $split2[$groupes[$key]['split_explode'][$k]]['erreur_DyT_SubC'] = '1';
+      }
+      if ($split2[$groupes[$key]['split_explode'][$k]]['DyT_Cust']<$split2[$groupes[$key]['split_explode'][$k-1]]['DyT_Cust'] OR $split2[$groupes[$key]['split_explode'][$k]]['DyT_Cust']<$split2[$groupes[$key]['split_explode'][$k-1]]['DyT_SubC']) {
+        $split2[$groupes[$key]['split_explode'][$k]]['erreur_DyT_Cust'] = '1';
+      }
     }
   }
+}
 
-
-
-
-
+//var_dump($split2);
 //var_dump($splits);
 //var_dump($groupes);
+
+
+// Rendre votre modèle accessible
+include 'models/schedule-model.php';
+// Création d'une instance
+$oSchedule = new SCHEDULE($db);
+$lstSchedule=$oSchedule->getAllSchedule($_GET['id_tbljob']);
 
 
 // Affichage du résultat
---
+++ controller/updateSchedule.php
@@ -0,0 +1,61 @@
+<?php
+include_once('../models/db.class.php'); // call db.class.php
+$db = new db(); // create a new object, class db()
+?>
+<?php
+
+
+
+
+
+
+
+// Rendre votre modèle accessible
+include '../models/schedule-model.php';
+// Création d'une instance
+$oSchedule = new SCHEDULE($db);
+
+
+//envoi des attributs du Schedule
+$oSchedule->dateSchedule=date('Y-m-d H:i:s');
+$oSchedule->schedule_commentaire=$_POST['schedule_commentaire'];
+$oSchedule->id_info_job=$_POST['id_info_job'];
+$oSchedule->schedule_recommendation=$_POST['schedule_recommendation'];
+
+$oSchedule->updateSchedule();
+
+
+
+
+
+
+//Pour la date de reception initiale (si non null)
+$formScheduleInitial = explode("&", $_POST['formScheduleInitial']);
+if ($formScheduleInitial[0]!="") {
+//on ne prend que le th (0) a cause du dédoublement des th avec datatables
+		$date = (explode("=", $formScheduleInitial[0])[1]=="")? "NULL" : explode("=", $formScheduleInitial[0])[1];
+
+		$oSchedule->updateScheduleInitial($date);
+	}
+
+
+//Pour chaque split
+$formScheduleSplit = explode("&", $_POST['formScheduleSplit']);
+if ($formScheduleSplit[0]!="") {
+	foreach ($formScheduleSplit as $key => $value) {
+		$arraysplit[explode("=", $value)[0]]['id']=explode("-", explode("=", $value)[0])[0];
+		$arraysplit[explode("=", $value)[0]]['type']=explode("-", explode("=", $value)[0])[1];
+		$arraysplit[explode("=", $value)[0]]['date'] = (explode("=", $value)[1]=="")? "NULL" : explode("=", $value)[1];
+
+	}
+//on regroupe les demandes ensembles (a cause du dédoublement des th avec datatables)
+	foreach ($arraysplit as $key => $value) {
+		$oSchedule->updateScheduleSplit($value['type'], $value['id'], $value['date']);
+	}
+
+}
+//echo 'en attendant que ca marche';
+
+$maReponse = array('id_tbljob' => $_POST['id_info_job']);
+echo json_encode($maReponse);
+?>
